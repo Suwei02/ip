dbeb2daf40531052994021ce6b6bf82cb6e367c2
Suwei <shresthasuwei@gmail.com>
2026-02-09 11:44:07 +0800

    Created Ui file for storing user interface printing methods.

--- src/main/java/speed/Speed.java
+++ src/main/java/speed/Speed.java
@@ -7,6 +7,8 @@ import speed.task.Todo;
 
 import java.util.Scanner;
 
+import  speed.ui.Ui;
+
 /**
  * Main class for the Speed application.
  */
@@ -15,78 +17,60 @@ public class Speed {
 
     private static final int MAX_TASKS = 100;
 
-    public static void printLine() {
-        System.out.println("____________________________________________________________");
-    }
-
-    private static final String EVENT_FORMAT_ERROR =
-            "Format: event <description> /from <start time> /to <end time>";
-    private static final String DEADLINE_FORMAT_ERROR =
-            "Format: deadline <description> /by <when>";
-
     public static void printList(Task[] tasks, int taskCount) {
         if (taskCount == 0) {
-            printLine();
+            Ui.printLine();
             System.out.println("NO TASKS YET! SIUUU!");
-            printLine();
+            Ui.printLine();
         } else {
-            printLine();
+            Ui.printLine();
             System.out.println("Here are the tasks in your list bro:  ");
             for (int i = 0; i < taskCount; i++) {
                 System.out.println((i + 1) + "." + tasks[i].toDisplayString());
             }
-            printLine();
+            Ui.printLine();
         }
     }
 
     public static void printAddedTask(Task task, int totalTaskCount) {
-        printLine();
+        Ui.printLine();
         System.out.println("Gotcha, I've added this task:");
         System.out.println(task.toDisplayString());
         System.out.println("Now you have " + totalTaskCount + " tasks in the list.");
-        printLine();
+        Ui.printLine();
     }
 
     public static void printMarkedTask(Task[] tasks, int markTaskIndex, int totalTaskCount) {
         if (!isValidTaskIndex(markTaskIndex, totalTaskCount) ) {
-            printLine();
+            Ui.printLine();
             System.out.println("Invalid task number man.");
-            printLine();
+            Ui.printLine();
         } else {
             tasks[markTaskIndex].markAsDone();
-            printLine();
+            Ui.printLine();
             System.out.println("HELL YEAH! ANOTHER TASK DONE:");
             System.out.println(tasks[markTaskIndex].toDisplayString());
-            printLine();
+            Ui.printLine();
         }
     }
 
     public static void printUnmarkedTask(Task[] tasks, int unmarkTaskIndex, int totalTaskCount) {
         if (!isValidTaskIndex(unmarkTaskIndex, totalTaskCount)) {
-            printLine();
+            Ui.printLine();
             System.out.println("Invalid task number man.");
-            printLine();
+            Ui.printLine();
         } else {
             tasks[unmarkTaskIndex].markAsNotDone();
-            printLine();
+            Ui.printLine();
             System.out.println("Ok, still waiting on this one bro:");
             //System.out.println("[ ] " + tasks[unmarkTaskIndex].getDescription());
             System.out.println(tasks[unmarkTaskIndex].toDisplayString());
 
-            printLine();
+            Ui.printLine();
         }
     }
 
-    private static void printEventFormatError() {
-        printLine();
-        System.out.println(EVENT_FORMAT_ERROR);
-        printLine();
-    }
-    private static void printDeadlineFormatError() {
-        printLine();
-        System.out.println(DEADLINE_FORMAT_ERROR);
-        printLine();
-    }
+
 
     // Helper to check validity of index
     private static boolean isValidTaskIndex(int idx, int total) {
@@ -94,7 +78,7 @@ public class Speed {
     }
 
     public static void main(String[] args) {
-        greet();
+        Ui.greet();
 
         Scanner scanner = new Scanner(System.in);
         Task[] tasks = new Task[MAX_TASKS];
@@ -103,7 +87,7 @@ public class Speed {
         while (true) {
             String input = scanner.nextLine().trim();
             if (input.equals("bye")) {
-                bye();
+                Ui.bye();
                 break;
 
             } else if (input.equals("list")) {
@@ -134,7 +118,7 @@ public class Speed {
 
                 //check format
                 if (parts.length < 2) {
-                    printDeadlineFormatError();
+                    Ui.printDeadlineFormatError();
                     continue;
                 }
 
@@ -150,7 +134,7 @@ public class Speed {
 
                 // Check format
                 if (parts.length < 3) {
-                    printEventFormatError();
+                    Ui.printEventFormatError();
                     continue;
                 }
 
@@ -162,34 +146,14 @@ public class Speed {
                 printAddedTask(tasks[totalTasksCount - 1], totalTasksCount);
 
             } else {
-                //tasks[totalTasksCount++] = new Task(input);
-                printLine();
-                System.out.println("invalid command.");
-                printLine();
+                Ui.printErrorMsg();
             }
         }
     }
 
-    private static void bye() {
-        printLine();
-        System.out.println("PEACE OUT BROTHER!🫡 GREEN APPLE!!!🍏🍏");
-        printLine();
-    }
 
-    public static void greet() {
-        String logo =
-                        "███████╗██████╗ ███████╗███████╗██████╗\n" +
-                        "██╔════╝██╔══██╗██╔════╝██╔════╝██╔══██╗\n" +
-                        "███████╗██████╔╝█████╗  █████╗  ██║  ██║\n" +
-                        "╚════██║██╔═══╝ ██╔══╝  ██╔══╝  ██║  ██║\n" +
-                        "███████║██║     ███████╗███████╗██████╔╝\n" +
-                        "╚══════╝╚═╝     ╚══════╝╚══════╝╚═════╝\n";
-        printLine();
-        System.out.println("YO! I'M SPEED 🏃💨");
-        System.out.println(logo);
-        System.out.println("WHAT CAN I DO FOR YOU BROOOO?? 💥💥💥");
-        printLine();
-    }
+
+
 }
 
 
