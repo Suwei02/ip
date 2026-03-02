63bdd613a585df5effdd29a95eb7b9a7a398260a
Suwei <shresthasuwei@gmail.com>
2026-03-02 10:07:52 +0800

    Implemented abstract class Command

--- src/main/java/speed/command/Command.java
+++ src/main/java/speed/command/Command.java
@@ -1,4 +1,32 @@
 package speed.command;
 
-public class Command {
+import speed.exception.SpeedException;
+import speed.storage.Storage;
+import speed.task.TaskList;
+import speed.ui.Ui;
+
+/**
+ * Represents an executable command in the Speed application.
+ * All specific command types inherit from this abstract class.
+ */
+public abstract class Command {
+    /**
+     * Executes the command with the given application components.
+     *
+     * @param tasks The task list to operate on.
+     * @param ui The UI for user interaction.
+     * @param storage The storage for saving tasks.
+     * @throws SpeedException If an error occurs during execution.
+     */
+    public abstract void execute(TaskList tasks, Ui ui, Storage storage) throws SpeedException;
+
+    /**
+     * Indicates whether this command should terminate the application.
+     * Default implementation returns false. Override in ExitCommand to return true.
+     *
+     * @return true if the application should exit, false otherwise.
+     */
+    public boolean isExit() {
+        return false;
+    }
 }
